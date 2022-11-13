679892e49752825d3ca8b6bb4b488dabeeb10386
SergeyShangin <sir.grey@list.ru>
2022-11-13 16:02:14 +0800

    prepare for merge and rebase 1

--- 02-git-03-branching/branching/rebase.sh
+++ 02-git-03-branching/branching/rebase.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-# display command line options for rebase
+# display command line options
 
 count=1
-for param in "$@"; do
-<<<<<<< HEAD
-    echo "Next parameter: $param"
-=======
-    echo "\$@ Parameter #$count = $param"
->>>>>>> 77000697ecabd677f9c4bc4394f958f5d7831c5f
+for param in "$*"; do
+    echo "\$* Parameter #$count = $param"
     count=$(( $count + 1 ))
 done
-
-echo "====="
